fec5157e112330a2a32334910ba0b45f77d16681
erica <75418479+ericaleee@users.noreply.github.com>
2022-01-15 15:06:43 +0800

    app

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
 }) ;
 
 UserSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",UserSchema);
+module.exports = mongoose.model("users",UserSchema);
 
 // var mongoose = require('mongoose');
 // var passportLocalMongoose = require("passport-local-mongoose");
